ff8ee95876f4abfac40660bf0f71e24b338b7c9f
ESIEA-Dive <dive@et.esiea.fr>
2022-09-05 12:29:55 +0200

    Structure + Layout

--- Modern-Site/src/App.jsx
+++ Modern-Site/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import styles from './style';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+// Import all the libraries using index.jx from ./components to look cleaner
 
-export default App
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
+
+//Learned alt +*2 click to select all and Ctrl Arrow right or left to add text everywhere
